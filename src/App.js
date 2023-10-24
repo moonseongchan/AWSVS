3e3728cce4be9dd99dfdde795657342f61cb627f
MSC <boyzmsc@snu.ac.kr>
2023-10-24 11:59:01 +0900

    feat: Fix Layout

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Dashboard from "./components/Dashboard";
 
 export default function App() {
   return (
-    <div class="container-fluid">
-      <Header />
-      <div class="row">
+    <div class="container-fluid h-100">
+      <Header/>
+      <div id="content" class="row">
         <Dashboard />
         <SideBar />
       </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import Image from "../../assets/logo_title.svg";
 
 const Header = (props) => {
   return (
-    <header class="row text-white">
+    <header class="row">
       <div class="col px-1">
         <img src={Image} alt="logo" height={55} />
       </div>
