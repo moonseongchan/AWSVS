f3523af551305af1a6ef805d8e197cc2b5b296a7
MSC <boyzmsc@snu.ac.kr>
2023-10-24 11:58:01 +0900

    feat: Add Dashboard Component

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import { BiChevronsLeft } from "react-icons/bi";
+
+import "./Dashboard.scss";
+
+const Dashboard = (props) => {
+  return (
+    <div class="col-md-9 p-2 border border-2 border-info">
+      <div class="d-flex flex-row-reverse pb-2">
+        <button type="button" class="new-slot-btn">
+          New Slot
+        </button>
+      </div>
+
+      <div class="slot w-100 border border-1 border-success mb-2">Slot 1</div>
+      <div class="slot w-100 border border-1 border-success mb-2">Slot 2</div>
+    </div>
+  );
+};
+
+export default Dashboard;
