abf07931d8c0b5fae8f4055e4a899307039580bc
MSC <boyzmsc@snu.ac.kr>
2023-11-17 21:50:11 +0900

    feat: Update Dashboard Spectrogram

--- src/components/Dashboard/spectrogram.js
+++ src/components/Dashboard/spectrogram.js
@@ -2,12 +2,17 @@ import React, { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 import { data2 } from "./data";
 
-const CustomSpectrogramComponent = (props) => {
-  const margin = {top: 20, right: 20, bottom: 40, left: 40};
+const Spectrogram = (props) => {
+  // To resize the width of the graph
+  const margin = { top: 10, right: 30, bottom: 20, left: 40 };
   const getGraphWidth = () => {
-    const sidebar = document.getElementById('sidebar-content').getBoundingClientRect().width;
+    const sidebar = document
+      .getElementById("sidebar-content")
+      .getBoundingClientRect().width;
     const isSidebarNext = window.innerWidth - sidebar < 30 ? false : true;
-    const width = isSidebarNext ? window.innerWidth * 0.69 : window.innerWidth * 0.88;
+    const width = isSidebarNext
+      ? window.innerWidth * 0.68
+      : window.innerWidth * 0.9;
     return width;
   };
   const handleResize = () => {
@@ -15,44 +20,92 @@ const CustomSpectrogramComponent = (props) => {
     setWidth(width);
   };
 
+  const spectrogramRef = useRef(null);
   const initialWidth = getGraphWidth() - margin.left - margin.right;
   const [width, setWidth] = useState(initialWidth);
-  const height = 200 - margin.bottom - margin.top;
-  const graphSvg = useRef();
+  const height = 200 - margin.top - margin.bottom;
 
   useEffect(() => {
-    let data = data2;
-    window.addEventListener('resize', handleResize);
-    const numRows = data.length;
-    const numCols = data[0].length;
-    const rect_width = width / numCols;
-    const rect_height = height / numRows;
+    window.addEventListener("resize", handleResize);
 
-    const colorScale = d3
-      .scaleSequential(d3.interpolateViridis)
-      .domain([
-        d3.min(data, (row) => d3.min(row)),
-        d3.max(data, (row) => d3.max(row)),
+    const svg = d3.select(spectrogramRef.current);
+    const raw = props.slot.data;
+    const plot = props.slot.plot;
+    const processing = props.slot.processing;
+    const options = props.slot.options;
+
+    if (plot.length > 0) {
+      const numRows = plot.length;
+      const numCols = plot[0].length;
+      const rect_width = width / numCols;
+      const rect_height = height / numRows;
+
+      // Domain => Time Series
+      const xScale = d3
+        .scaleLinear()
+        .domain([0, plot[0].length])
+        .range([0, width]);
+
+      let colorScale;
+      if (options.spectrogramColor === "Viridis") {
+        colorScale = d3.scaleSequential(d3.interpolateViridis);
+      } else if (options.spectrogramColor === "Cividis") {
+        colorScale = d3.scaleSequential(d3.interpolateCividis);
+      } else if (options.spectrogramColor === "Plasma") {
+        colorScale = d3.scaleSequential(d3.interpolatePlasma);
+      } else if (options.spectrogramColor === "Turbo") {
+        colorScale = d3.scaleSequential(d3.interpolateTurbo);
+      } else if (options.spectrogramColor === "CubehelixDefault") {
+        colorScale = d3.scaleSequential(d3.interpolateCubehelixDefault);
+      } else if (options.spectrogramColor === "Inferno") {
+        colorScale = d3.scaleSequential(d3.interpolateInferno);
+      }
+
+      colorScale.domain([
+        d3.min(plot, (row) => d3.min(row)),
+        d3.max(plot, (row) => d3.max(row)),
       ]);
 
-    const svg = d3.select(graphSvg.current);
+      svg.selectAll("*").remove();
 
-    for (let i = 0; i < numRows; i++)
-      for (let j = 0; j < numCols; j++)
-        svg
-          .append("rect")
-          .attr("x", j * rect_width)
-          .attr("y", i * rect_height)
-          .attr("width", rect_width)
-          .attr("height", rect_height)
-          .style("fill", colorScale(data[i][j]))
-          .attr("transform", `translate(${margin.left}, 0)`);
-  }, [width]);
+      const graph = svg
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+      for (let i = 0; i < numRows; i++)
+        for (let j = 0; j < numCols; j++)
+          graph
+            .append("rect")
+            .attr("x", j * rect_width)
+            .attr("y", i * rect_height)
+            .attr("width", rect_width)
+            .attr("height", rect_height)
+            .style("fill", colorScale(plot[i][j]));
+
+      // Axis
+      svg
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top + height})`)
+        .call(d3.axisBottom(xScale));
+    }
+  }, [width, props.slot]);
+
   return (
-    <div>
-      <svg ref={graphSvg} width={width} height={height}></svg>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <svg
+        ref={spectrogramRef}
+        width={width + margin.left + margin.right}
+        height={height + margin.top + margin.bottom}
+      ></svg>
     </div>
   );
 };
 
-export default CustomSpectrogramComponent;
+export default Spectrogram;
